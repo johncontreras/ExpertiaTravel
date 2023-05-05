a1869ba1718ebdd95c462b3d9b36a54d475e2a42
John Alex Contreras Soto <johnalex.cs@gmail.com>
2023-05-05 10:30:02 -0500

    Update NMV/Vuelos/06 nmv_vuelos_checkout_seleccionarPago.js

--- NMV/Vuelos/06 nmv_vuelos_checkout_seleccionarPago.js
+++ NMV/Vuelos/06 nmv_vuelos_checkout_seleccionarPago.js
@@ -15,7 +15,7 @@ window.dataLayer.push({
     email:"annie_andrade@gmail.com",
     primerNombre:"Annie",
     primerApellido:"Andrade",
-    fechaNacimiento:"21/12/1993",
+    fechaNacimiento:"1993-12-21",
     nacionalidad:"Peru",
     genero: "femenino",
     documento_tipo:"DNI",
