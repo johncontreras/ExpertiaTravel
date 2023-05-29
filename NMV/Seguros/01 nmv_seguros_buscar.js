7b8006796604fb2ce6787c150ce077c53f082f0f
John Alex Contreras Soto <johnalex.cs@gmail.com>
2023-05-29 17:00:05 -0500

    Update NMV/Seguros/01 nmv_seguros_buscar.js

--- NMV/Seguros/01 nmv_seguros_buscar.js
+++ NMV/Seguros/01 nmv_seguros_buscar.js
@@ -1,34 +1,8 @@
-//Evento Equivalente: 
-
-
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
   event: "nmv_seguros_buscar",
   operacion: {
-    id: "1104456", // Código SRV o equivalente con el que se guarda esta transaccion.
     dias_anticipacion: 10, //
-  },
-  precio: {
-    moneda: "USD",
-    precioNormal: 32.00,
-    precioFinal: 32.00,
-  },
-  metodo_pago: {
-    opcion: "Safety Pay",
-  },
-  usuario:{
-    email:"annie_andrade@gmail.com",
-    primerNombre:"Annie",
-    primerApellido:"Andrade",
-    fechaNacimiento:"21/12/1993",
-    nacionalidad:"Peru",
-    genero: "femenino",
-    documento_tipo:"DNI",
-    documento_numero:"70014526",
-    telefono_tipo:"celular",
-    telefono_codigo:"51",
-    telefono_numero:"940663173",
-    frecuencia:"pasajero-frecuente", // primera-reserva
   },
   origen: {
     nombre: "Perú",
@@ -41,7 +15,7 @@ window.dataLayer.push({
   pasajeros: {
     adultos: 5,
     ninos: 1,
-    infantes: 0,
+    infantes: 0, // hasta 5 años
     total: 6,
   },
   fechas: {
